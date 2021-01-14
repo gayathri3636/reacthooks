e10d0637afc2e83509cc5103c608ecd99b13dd36
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2021-01-13 21:03:15 -0500

    usestate with arrays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 // import UsestateCounter from './components/usestateCounter';
 // import UseStateCounterTwo from './components/UseStateCounterTwo';
-import UseStateCounterThree from './components/UseStateCounterThree';
+// import UseStateCounterThree from './components/UseStateObjects';
+import UsestateArrays from './components/UsestateArrays';
+
 function App() {
   return (
     <div className="App">
       {/* <UsestateCounter /> */}
       {/* <UseStateCounterTwo /> */}
-      <UseStateCounterThree />
+      {/* <UseStateCounterThree /> */}
+      <UsestateArrays />
     </div>
   );
 }
