d1218e3d33f9306497d537398c92fb3d9c1427d9
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2021-01-15 11:26:57 -0500

    datafetching part1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import './App.css';
-import React, {useReducer} from 'react';
-import ComponentA from './components/useReducer/ComponentA';
-import ComponentB from './components/useReducer/ComponentB';
-import ComponentC from './components/useReducer/ComponentC';
+import React from 'react';
+import DataFetchingOne from './components/useReducer/DataFetchingOne';
+// import ComponentA from './components/useReducer/ComponentA';
+// import ComponentB from './components/useReducer/ComponentB';
+// import ComponentC from './components/useReducer/ComponentC';
 
-export const countContext = React.createContext()
-const initialState = 0;
-const reducer = (state, action) => {
-  switch(action){
-      case 'increment':
-          return state+1;
-      case 'decrement':
-          return state-1;
-      case 'reset':
-          return initialState
-      default:
-          return state;
-  }
+// export const countContext = React.createContext()
+// const initialState = 0;
+// const reducer = (state, action) => {
+//   switch(action){
+//       case 'increment':
+//           return state+1;
+//       case 'decrement':
+//           return state-1;
+//       case 'reset':
+//           return initialState
+//       default:
+//           return state;
+//   }
+// }
+
+// function App() {
+//   const [count, dispatch] = useReducer(reducer, initialState)
+//   return (
+//     <countContext.Provider value={{countState:count, countDispatch:dispatch}}>
+//       <div className="App">
+//         <h1>count in App.js- {count}</h1>
+//         <ComponentA />
+//         <ComponentB />
+//         <ComponentC />
+//       </div>
+//     </countContext.Provider>
+//   );
+// }
+
+// export default App;
+
+function App(){
+  return(
+    <div>
+      <DataFetchingOne />
+    </div>
+  )
 }
-
-function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
-  return (
-    <countContext.Provider value={{countState:count, countDispatch:dispatch}}>
-      <div className="App">
-        <h1>count in App.js- {count}</h1>
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </div>
-    </countContext.Provider>
-  );
-}
-
 export default App;
